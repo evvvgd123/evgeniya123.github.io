52fee14be9ae85f2a2ab3551045b0d6ddaa9f82b
evvvgd123 <dubnova2005@mail.ru>
2026-04-13 16:02:11 +0300

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: "/evgeniya123.github.io",
+  assetPrefix: "/evgeniya123.github.io",
 };
 
 export default nextConfig;
